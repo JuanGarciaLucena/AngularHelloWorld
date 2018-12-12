57c91f474a425937a0a4a771a4b06bf13621413b
juanantonio.garcia <juanantonio.garcia@sdos.es>
2018-12-12 14:46:58 +0100

    parallax added

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
+
+    
+      $('.parallax').parallax();
   }
 
 }
